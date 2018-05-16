71f169d7dc95106585a06cd68c26383f6d60c731
sergey144010 <sergey144010@mail.ru>
2018-05-16 23:40:31 +0800

    Add exceptions

---
+++ src/Exception/ErrorInsertIntoTasks.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Мария
+ * Date: 16.05.2018
+ * Time: 2:55
+ */
+
+namespace sergey144010\tasks\Exception;
+
+
+class ErrorInsertIntoTasks extends TaskException
+{
+
+}
---
+++ src/Exception/NotFoundException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Мария
+ * Date: 16.05.2018
+ * Time: 2:22
+ */
+
+namespace sergey144010\tasks\Exception;
+
+
+class NotFoundException extends TaskException
+{
+
+}
